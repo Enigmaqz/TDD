1ff872efbb3532502500d9fb95db69e824b9affb
Boffka92 <boffka92@ya.ru>
2023-11-06 18:01:53 +0300

    code improved

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -3,31 +3,31 @@ package org.example;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class PhoneBook {
 
     private HashMap<String, String> phoneBook = new HashMap<>();
-    public int add(String name, String number){
+
+    public int add(String name, String number) {
         phoneBook.put(name, number);
         return phoneBook.size();
     }
 
-    public String findByNumber (String number) {
-        String result = null;
-        for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
-            if (Objects.equals(number, entry.getValue())) {
-                result =  entry.getKey();
-                break;
-            }
-        }
-    return result;
+    public String findByNumber(String number) {
+        return phoneBook
+                .entrySet()
+                .stream()
+                .filter(entry -> number.equals(entry.getValue()))
+                .map(Map.Entry::getKey)
+                .findFirst().get();
     }
 
-    public String findByName (String name) {
+    public String findByName(String name) {
         return phoneBook.get(name);
     }
 
-    public String printAllNames () {
+    public String printAllNames() {
         String result = null;
 
         StringBuilder stringBuilder = new StringBuilder();
