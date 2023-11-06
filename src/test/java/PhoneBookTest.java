f180729e6f95d34274c266ebadf2aae3c04787d5
Boffka92 <boffka92@ya.ru>
2023-11-06 17:12:50 +0300

    Stub and tests for findByNumber

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -5,13 +5,13 @@ import org.junit.Test;
 public class PhoneBookTest {
 
     @Test
-    public void testAddFirst(){
-        PhoneBook phoneBook=new PhoneBook();
+    public void testAddFirst() {
+        PhoneBook phoneBook = new PhoneBook();
         String name1 = "Bob";
-        String number1 ="1111";
+        String number1 = "1111";
 
         int result;
-        result = phoneBook.add (name1, number1);
+        result = phoneBook.add(name1, number1);
 
         int expected = 1;
 
@@ -19,24 +19,41 @@ public class PhoneBookTest {
     }
 
     @Test
-    public void testAddDouble(){
-        PhoneBook phoneBook=new PhoneBook();
+    public void testAddDouble() {
+        PhoneBook phoneBook = new PhoneBook();
         String name1 = "Bob";
-        String number1 ="1111";
+        String number1 = "1111";
 
         String name2 = "Ann";
-        String number2 ="2222";
+        String number2 = "2222";
 
         String name3 = "Bob";
-        String number3 ="3333";
+        String number3 = "3333";
 
         int result;
-        result = phoneBook.add (name1, number1);
-        result = phoneBook.add (name2, number2);
-        result = phoneBook.add (name3, number3);
+        result = phoneBook.add(name1, number1);
+        result = phoneBook.add(name2, number2);
+        result = phoneBook.add(name3, number3);
 
         int expected = 2;
 
         Assert.assertEquals(expected, result);
     }
+
+    @Test
+    public void testFindByNumber() {
+        PhoneBook phoneBook = new PhoneBook();
+        String name1 = "Bob";
+        String number1 = "1111";
+
+
+        phoneBook.add(name1, number1);
+        String result;
+        result = phoneBook.findByNumber(name1);
+
+        String expected = "1111";
+
+        Assert.assertEquals(expected, result);
+    }
+
 }
